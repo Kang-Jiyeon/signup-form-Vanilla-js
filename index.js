1803b1fed086e427a0cf67ea41b8b5ed25b0bc43
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-03-10 21:01:36 +0900

    230310 update
    [추가] 인증 폼 유효성 검사 로직

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const $id = document.getElementById("id");
 window.addEventListener("load", () => $id.focus());
+
+const $pw = document.getElementById("pw");
+const $pwCheck = document.getElementById("pw-check");
+const $submit = document.getElementById("submit");
+
+const ID_REGEX = new RegExp("^[a-z0-9_-]{5,20}$");
+const PW_REGEX = new RegExp("^[a-zA-Z0-9]{8,16}$");
+
+const checkIdValidation = (value) => {
+  const isValidId = ID_REGEX.test(value);
+  console.log(isValidId);
+};
+
+const checkPwValidation = (value) => {
+  const isValidPw = PW_REGEX.test(value);
+  console.log(isValidPw);
+};
+
+const checkPwCheckValidation = (value) => {
+  const isValidPwCheck = $pw.value === value;
+  console.log(isValidPwCheck);
+};
+
+$id.addEventListener("focusout", () => checkIdValidation($id.value));
+$pw.addEventListener("focusout", () => checkPwValidation($pw.value));
+$pwCheck.addEventListener("focusout", () =>
+  checkPwCheckValidation($pwCheck.value)
+);
+$submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkIdValidation($id.value);
+  checkPwValidation($pw.value);
+  checkPwCheckValidation($pwCheck.value);
+});
